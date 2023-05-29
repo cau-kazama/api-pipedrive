ac85531249023952c9d139511b4e328edceba5f3
caukazamapj <cauankazama@usp.br>
2023-05-29 19:56:03 -0300

    fdksaf

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.get("/", function (req, res) {
 app.post("/pipedrive/webhook", async function (req, res) {
     const {id, status } = req.body.current;
 
+    console.log(req.body.current);
+    
     // if(status === 'won'){
         var response = await fetch(`https://api.pipedrive.com/v1/deals/${id}?api_token=` + pipedriveAPIKey);
         var data = response.json();
