eee8c41695711da6d89fa69774615b17e7221cfe
caukazamapj <cauankazama@usp.br>
2023-05-29 16:45:06 -0300

    added pipedrive api and sheets integration

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const app = express()
 
+var pipedriveAPIKey = '30fefb0f89944597b0a401a190df6f949e32ac4b';
+var sheetID = '1x6x5jHiAe66SQXzjk6mUuve5B9DAzylWsT0YzMgiTFg';
+var sheetName = 'Página1';
 // use the express-static middleware
 app.use(express.static("public"))
 
@@ -10,11 +13,67 @@ app.use(express.json());
 // define the first route
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
-  console.log('asfsdfd');
 })
 
 app.post("/pipedrive/webhook", function (req, res) {
-    console.log(req.body.current)    
+    const {id, status } = req.body.current;
+
+    // if(status === 'won'){
+        var response = UrlFetchApp.fetch(`https://api.pipedrive.com/v1/deals/${id}?status=won&api_token=` + pipedriveAPIKey);
+        var data = JSON.parse(response.getContentText());
+        var latestDeal = data.data;
+
+        var sheet = SpreadsheetApp.openById(sheetID).getSheetByName(sheetName);
+        var lastRow = sheet.getLastRow();
+        var newRow = lastRow + 1;
+
+        var organizationName = latestDeal.org_id ? latestDeal.org_id.name : 'N/A';
+
+
+        var dealField = UrlFetchApp.fetch(`https://api.pipedrive.com/v1/dealFields?status=won&api_token=` + pipedriveAPIKey);
+
+        var fieldData = JSON.parse(dealField).data
+
+        var fieldOrigem = fieldData.find((field) => 
+        field.name === 'Origem'
+        )
+
+        var fieldLabel = fieldData.find((field) => field.name === 'Etiqueta');
+
+        var email = latestDeal.user_id.email;
+        var vendedor = email.slice(0, email.indexOf('@'));
+
+        var origemName = fieldOrigem.options.find((option) => option.id === Number(latestDeal['97d0502cc2b489986844a93b374656e5acf179e1'])).label
+
+        var nucleoName = fieldLabel.options.find((option) => option.id === Number(latestDeal.label)).label
+
+        var wonDate = new Date(latestDeal.won_time)
+
+        var wonDateString = `${wonDate.getDate()}/${wonDate.getUTCMonth() + 1}/${wonDate.getFullYear()}`
+
+        var buyer = latestDeal.person_id;
+
+        var buyerName = buyer.name;
+
+        var buyerEmail = buyer.email[0].value;
+
+        var buyerPhone = buyer.phone[0].value;
+
+        var rowData = [
+            latestDeal.title,
+            nucleoName,
+            latestDeal.value,
+            origemName,
+            organizationName,
+            wonDateString,
+            vendedor,
+            buyerName,
+            buyerEmail,
+            buyerPhone
+        ];
+
+        sheet.getRange(newRow, 1, 1, rowData.length).setValues([rowData]);
+    // }
 })
 
 // start the server listening for requests
